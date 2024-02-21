7e95df9611bc47715044e5d2fae79c61a69b778c
Oluwatobi Bamidele <teebams49@gmail.com>
2024-02-21 13:25:43 +0100

    feat: navigation section

---
+++ src/components/navMenu.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+
+interface Props {
+  name: string;
+  sectionName: string;
+}
+
+const scrollToSection = (id: string) => {
+  const element = document.getElementById(id);
+  if (element) {
+    element.scrollIntoView({
+      behavior: "smooth",
+    });
+  }
+};
+
+const NavMenu = ({ name, sectionName }: Props) => {
+  return (
+    <div
+      className="flex py-0 px-[0.5rem] items-center justify-center gap-[0.5rem]"
+      onClick={() => scrollToSection(sectionName)}
+    >
+      <Link
+        href={`#${sectionName}`}
+        className="font-normal text-base leading-[1.5rem] text-black font-sans hover:text-primary hover:text-[1.01rem]"
+      >
+        {name}
+      </Link>
+    </div>
+  );
+};
+
+export default NavMenu;
---
+++ src/components/navigation.tsx
@@ -0,0 +1,49 @@
+"use client";
+import { useEffect } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import NavMenu from "./navMenu";
+
+const scrollToSection = (id: string) => {
+  const element = document.getElementById(id);
+  if (element) {
+    element.scrollIntoView({
+      behavior: "smooth",
+    });
+  }
+};
+
+const Navigation = () => {
+  useEffect(() => {
+    const hash = window.location.hash;
+    if (hash) {
+      scrollToSection(hash.substring(1));
+    }
+  }, []);
+
+  return (
+    <nav
+      className="w-full flex justify-between items-center px-[6.5rem] pt-[1.5625rem] pb-[1.5rem] bg-white shadow-md"
+      style={{ boxShadow: "0px 2px 0px 0px rgba(0, 7, 47, 0.10)" }}
+    >
+      <Link
+        className="flex items-center justify-center cursor-pointer"
+        onClick={() => scrollToSection("home")}
+        href="/"
+      >
+        <Image src={"/logo.svg"} width={83} height={47} alt={""} />
+      </Link>
+      <div className=" inline-flex items-start gap-[2.5rem]">
+        <NavMenu name="About Us" sectionName="about" />
+        <NavMenu name="Our Founders" sectionName="founders" />
+        <NavMenu name="Advisory Board" sectionName="board" />
+        <NavMenu name="Resources" sectionName="resources" />
+      </div>
+      <button className="bg-primary text-white px-[1.2rem] py-[0.875rem] rounded-lg gap-2">
+        Get Involved
+      </button>
+    </nav>
+  );
+};
+
+export default Navigation;
