908ef16fc683e00c894d822f38d4d376b40a45bc
Oluwatobi Bamidele <teebams49@gmail.com>
2024-02-26 14:44:12 +0100

    update: made footer responsive

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -9,21 +9,55 @@ const Footer = () => {
   return (
     <footer className="flex flex-col py-16 items-center justify-center bg-dark-green border-t-[1px] border-solid border-white">
       <div className="flex flex-col items-center gap-8">
-        <div className="flex items-center justify-center gap-10">
+        <div className="flex flex-col lg:flex-row items-center justify-center gap-10">
+          <div className="flex flex-col items-center justify-center gap-2 lg:hidden">
+            <Image
+              src={"/student-network.svg"}
+              alt={"Student Network"}
+              height={0}
+              width={0}
+              sizes="100vw"
+              className="w-[12.25rem] h-[0.74119rem]"
+            />
+            <p className="text-white text-center text-sm leading-[150%]">
+              An initiative of Generation Bitcoin 501(c)(3)
+            </p>
+          </div>
+          <div className="flex items-center justify-center gap-[1.21994rem]">
+            <Image
+              src={"/genb.png"}
+              alt="GenB"
+              height={0}
+              width={0}
+              sizes="100vw"
+              className="w-[5.48963rem] h-[2.389rem] lg:hidden"
+            />
+            <Image
+              src={"/btrust.svg"}
+              alt={"Student Network"}
+              height={0}
+              width={0}
+              sizes="100vw"
+              className="w-[5.54044rem] h-[2.389rem] lg:hidden"
+            />
+          </div>
           <Image
             src={"/genb.png"}
             alt="GenB"
             height={0}
             width={0}
             sizes="100vw"
-            className="w-[6.75rem] h-[2.9375rem]"
+            className="w-[6.75rem] h-[2.9375rem] hidden lg:flex"
           />
-          <div className="flex flex-col items-center justify-center gap-2">
-            <h2
-              className={`text-white text-xl font-semibold ${grandisBold.className}`}
-            >
-              Bitcoin Student Network
-            </h2>
+          <div className="flex-col items-center justify-center gap-2 hidden lg:flex">
+            <Image
+              src={"/student-network.svg"}
+              alt={"Student Network"}
+              height={0}
+              width={0}
+              sizes="100vw"
+              className="w-[14.75581rem] h-[0.89281rem]"
+            />
             <p className="text-white text-center text-base leading-[150%]">
               An initiative of Generation Bitcoin 501(c)(3)
             </p>
@@ -34,31 +68,31 @@ const Footer = () => {
             height={0}
             width={0}
             sizes="100vw"
-            className="w-[6.8125rem] h-[2.9375rem]"
+            className="w-[6.8125rem] h-[2.9375rem] hidden lg:flex"
           />
         </div>
         <div className="flex flex-col items-center gap-4">
           <div className="w-[6rem] h-[0.125rem] bg-primary rounded"></div>
-          <div className="flex items-center justify-center gap-6">
+          <div className="flex flex-col lg:flex-row items-center justify-center gap-10 lg:gap-6">
             <NavMenu
               name="About Us"
               sectionName="about"
-              className="text-white"
+              className="text-white text-sm"
             />
             <NavMenu
               name="Our Founders"
               sectionName="founders"
-              className="text-white"
+              className="text-white text-sm"
             />
             <NavMenu
               name="Advisory Board"
               sectionName="board"
-              className="text-white"
+              className="text-white text-sm"
             />
             <NavMenu
               name="Resources"
               sectionName="resources"
-              className="text-white"
+              className="text-white text-sm"
             />
           </div>
         </div>
