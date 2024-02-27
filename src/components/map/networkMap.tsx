ad42b4f6a877d167bdda699b6ee52095a1a894fd
Oluwatobi Bamidele <teebams49@gmail.com>
2024-02-27 10:51:33 +0100

    update: updated map location

--- src/components/map/networkMap.tsx
+++ src/components/map/networkMap.tsx
@@ -15,13 +15,21 @@ const NetworkMap = () => {
     const map = new jsVectorMap({
       selector: "#map",
       map: "world",
+      regionStyle: {
+        initial: {
+          fill: "#004915",
+        },
+        hover: {
+          fill: "#A0D1DC",
+        },
+      },
       zoomMax: 1, // Set the maximum zoom level to 1 to disable zooming
       zoomButtons: false, // Disable zoom buttons
       markerStyle: {
         initial: {
           image: "/marker.svg",
-          width: "1.5rem", // Set the width of the marker image
-          height: "1.5rem",
+          width: "1.2rem", // Set the width of the marker image
+          height: "1.2rem",
         },
         hover: {
           "stroke-width": 2,
@@ -34,31 +42,32 @@ const NetworkMap = () => {
       markerLabelStyle: {
         initial: {
           // Add CSS properties here
-          fill: "#000",
-          fontSize: "0.875rem",
+          fill: "#fff",
+          fontSize: "0.75rem",
           fontFamily: "Inter",
+          fontWeight: "normal",
         },
       },
       // Other configuration options for jsvectormap
       markers: [
         {
           name: "UK",
-          coords: [55.3781, 3.436],
+          coords: [53.3781, -3.436],
         },
         {
           name: "United States",
-          coords: [37.0902, -95.7129],
+          coords: [39.0902, -98.7129],
         },
-        { name: "Canada", coords: [45.4215, -75.6994] },
-        { name: "Japan", coords: [35.6895, 139.6917] },
-        { name: "Mexico", coords: [19.4326, -99.1332] },
-        { name: "Italy", coords: [41.9028, 12.4964] },
-        { name: "Brazil", coords: [-15.7801, -47.9292] },
-        { name: "Spain", coords: [40.4168, -3.7038] },
-        { name: "Australia", coords: [-35.2809, 149.13] },
+        { name: "Canada", coords: [60.4215, -105.6994] },
+        { name: "Japan", coords: [42.6895, 142.6917] },
+        { name: "Mexico", coords: [21.4326, -99.1332] },
+        { name: "Italy", coords: [44.9028, 12.4964] },
+        { name: "Brazil", coords: [-10.7801, -50.9292] },
+        { name: "Spain", coords: [38.4168, -3.7038] },
+        { name: "Australia", coords: [-25.2809, 130.13] },
         { name: "Ghana", coords: [5.6037, -0.187] },
         { name: "South Korea", coords: [37.5665, 126.978] },
-        { name: "Taiwan", coords: [25.033, 121.5654] },
+        { name: "Taiwan", coords: [23.6978, 120.9605] },
       ],
       labels: {
         markers: {
@@ -101,7 +110,7 @@ const NetworkMap = () => {
   }, []);
 
   return (
-    <div className="flex py-8 lg:py-16 px-4 lg:px-[6.5rem] flex-col justify-center items-center gap-2 w-full">
+    <div className="flex py-8 lg:py-16 px-4 lg:px-[6.5rem] flex-col justify-center bg-dark-green items-center gap-2 w-full">
       <div className="flex flex-col items-center gap-8 w-full">
         <Heading2 text="Map of Our Nodes" className="text-center" />
         <div className={style["map-container"]}>
