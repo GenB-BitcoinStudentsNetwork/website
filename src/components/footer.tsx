5ee876e4f55d83244090678346f142818ad3bbfb
Oluwatobi Bamidele <teebams49@gmail.com>
2024-02-28 23:05:49 +0100

    update: updated footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -10,66 +10,19 @@ const Footer = () => {
     <footer className="flex flex-col py-16 items-center justify-center bg-dark-green border-t-[1px] border-solid border-white">
       <div className="flex flex-col items-center gap-8">
         <div className="flex flex-col lg:flex-row items-center justify-center gap-10">
-          <div className="flex flex-col items-center justify-center gap-2 lg:hidden">
+          <div className="flex flex-col items-center justify-center gap-2">
             <Image
               src={"/student-network.svg"}
               alt={"Student Network"}
               height={0}
               width={0}
               sizes="100vw"
-              className="w-[12.25rem] h-[0.74119rem]"
+              className="w-[12.25rem] h-[0.74119rem] lg:w-[14.75581rem] lg:h-[0.89281rem]"
             />
-            <p className="text-white text-center text-sm leading-[150%]">
+            <p className="text-white text-center text-sm lg:text-base leading-[150%]">
               An initiative of Generation Bitcoin 501(c)(3)
             </p>
           </div>
-          <div className="flex items-center justify-center gap-[1.21994rem]">
-            <Image
-              src={"/genb.png"}
-              alt="GenB"
-              height={0}
-              width={0}
-              sizes="100vw"
-              className="w-[5.48963rem] h-[2.389rem] lg:hidden"
-            />
-            <Image
-              src={"/btrust.svg"}
-              alt={"Student Network"}
-              height={0}
-              width={0}
-              sizes="100vw"
-              className="w-[5.54044rem] h-[2.389rem] lg:hidden"
-            />
-          </div>
-          <Image
-            src={"/genb.png"}
-            alt="GenB"
-            height={0}
-            width={0}
-            sizes="100vw"
-            className="w-[6.75rem] h-[2.9375rem] hidden lg:flex"
-          />
-          <div className="flex-col items-center justify-center gap-2 hidden lg:flex">
-            <Image
-              src={"/student-network.svg"}
-              alt={"Student Network"}
-              height={0}
-              width={0}
-              sizes="100vw"
-              className="w-[14.75581rem] h-[0.89281rem]"
-            />
-            <p className="text-white text-center text-base leading-[150%]">
-              An initiative of Generation Bitcoin 501(c)(3)
-            </p>
-          </div>
-          <Image
-            src={"/btrust.svg"}
-            alt="Btrust"
-            height={0}
-            width={0}
-            sizes="100vw"
-            className="w-[6.8125rem] h-[2.9375rem] hidden lg:flex"
-          />
         </div>
         <div className="flex flex-col items-center gap-4">
           <div className="w-[6rem] h-[0.125rem] bg-primary rounded"></div>
@@ -109,9 +62,19 @@ const Footer = () => {
               name="tree"
             />
           </div>
-          <p className="text-white text-center text-base leading-[150%]">
-            ©BSN {currentYear} | Built by GADA Studios
-          </p>
+          <div className="flex items-center justify-center gap-1 lg:gap-2">
+            <p className="text-white text-center text-base leading-[150%]">
+              ©BSN {currentYear} | Built by GADA Studios |
+            </p>
+            <Image
+              src={"/btrust.svg"}
+              alt="Btrust"
+              height={0}
+              width={0}
+              sizes="100vw"
+              className="w-[3.25rem] h-[1.375rem] lg:w-[3.25rem] lg:h-[1.375rem]"
+            />
+          </div>
         </div>
       </div>
     </footer>
