9646155df46fea825310a1f6353984a77936bdd2
Oluwatobi Bamidele <teebams49@gmail.com>
2024-02-29 11:51:46 +0100

    update: center departments on mobile view

--- src/components/board/boardDepartmentLayout.tsx
+++ src/components/board/boardDepartmentLayout.tsx
@@ -8,11 +8,11 @@ interface Props {
 }
 const BoardDepartment = ({ children, title }: Props) => {
   return (
-    <div className="flex flex-col items-start justify-between gap-4 w-full">
+    <div className="flex flex-col items-center lg:items-start justify-between gap-4 w-full">
       <h3 className="text-black text-lg lg:text-2xl font-bold leading-[150%]">
         {title}
       </h3>
-      <div className="flex items-start gap-[0.94rem] lg:gap-6 w-full">
+      <div className="flex items-start gap-[0.94rem] lg:gap-6 w-full justify-center">
         {children}
       </div>
     </div>
