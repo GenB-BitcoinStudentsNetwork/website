68b453219de75789e62cd4b672135febba9a9b28
Oluwatobi Bamidele <teebams49@gmail.com>
2025-01-05 21:55:47 +0100

    chore: update faq

--- src/components/layerZero/faq.tsx
+++ src/components/layerZero/faq.tsx
@@ -10,56 +10,80 @@ export default function Faq() {
     },
     {
       question: "Who can apply to Layer Zero?",
-      answer: "Who can apply to Layer Zero?",
+      answer:
+        "The program is open to all undergraduate-aged students with a passion for decentralized innovation. Applicants must be 18 or older, motivated to learn, and committed to the program’s mission.",
     },
     {
       question: "What is the duration of the program?",
-      answer: "What is the duration of the program?",
+      answer:
+        "The program lasts approximately 2.5 months, from June 1 to August 15, depending on the project, location, and student’s (academic) schedule.",
     },
     {
       question: "Is this an all-expenses-paid program?",
-      answer: "Is this an all-expenses-paid program?",
+      answer:
+        "Yes! All expenses for travel, accommodation, food, and other potential logistics costs are fully covered, allowing participants to focus entirely on their internship without financial burden. This is a unique opportunity for students to immerse themselves in the Bitcoin ecosystem while receiving full financial support.",
     },
     {
       question: "Where will I be working?",
-      answer: "Where will I be working?",
+      answer:
+        "Participants will work in global locations where Bitcoin adoption and financial inclusion can have the most impact. These locations may include emerging markets or areas with limited access to traditional banking services. Specific locations will be assigned based on the needs of local Bitcoin communities and participant preferences, ranging from circular economies to operational, technical and infrastructure-related projects.",
     },
     {
       question: "What kind of projects will I be working on?",
-      answer: "What kind of projects will I be working on?",
+      answer:
+        "These are not behind desk jobs. You will be furthering bitcoin adoption through in the field operational initiatives which could include teaching local communities about Bitcoin, helping set up local Bitcoin payment systems,  and contributing to real world applications powered by Bitcoin. The specific project you’ll be involved in will depend on the community’s needs.",
     },
     {
       question: "Do I need to speak another language?",
-      answer: "Do I need to speak another language?",
+      answer:
+        "Multilingual skills are a plus, but basic English and translation tools are sufficient for most assignments.",
     },
     {
       question: "What kind of skills do I need?",
-      answer: "What kind of skills do I need?",
+      answer:
+        "Participants should demonstrate open-mindedness, a willingness to take on any task, and the ability to leave their ego at the door. A strong team-player attitude, a commitment to proof of work, and adaptability are key. Additionally, being passionate about global collaboration, acting with integrity, and equipped with excellent communication and problem-solving skills are essential for success.",
     },
-    { question: "How do I apply?", answer: "How do I apply?" },
+    {
+      question: "How do I apply?",
+      answer: "Visit the 'Apply Now' page",
+    },
     {
       question: "What is the selection process?",
-      answer: "What is the selection process?",
+      answer:
+        "Applications are reviewed, followed by interviews. Final decisions are based on alignment with program goals.",
+    },
+    {
+      question: "How will I be matched to an “internship?”",
+      answer:
+        "We’re here to connect your unique skills and passions with the right community, creating opportunities for growth, real collaboration, and lasting impact. It’s about finding the perfect fit where you can thrive and make a difference together.",
     },
     {
       question: "What will I gain from this experience?",
-      answer: "What will I gain from this experience?",
+      answer:
+        "You’ll gain hands-on experience with the communities reshaping society and experience in the language of the future, while honing cross-cultural communication skills. This is an excellent opportunity to build your professional network by connecting with peers who are shaping the future. Additionally, you'll develop leadership and problem-solving skills, enhance your adaptability in diverse environments, and gain a standout experience to strengthen your resume and career trajectory.",
     },
     {
       question: "What happens if I encounter challenges?",
-      answer: "What happens if I encounter challenges?",
+      answer:
+        "The program is designed to support you every step of the way. You'll have access to experienced mentors who can offer guidance and insights, coordinators who help address logistical or program-related concerns, and a supportive network of peers to brainstorm solutions and share experiences. Whether you're facing technical difficulties, cultural adjustments, or project-related hurdles, you'll be equipped with the tools, resources, and a collaborative community to navigate challenges and grow from the experience.",
     },
     {
       question: "Can I return early if needed?",
-      answer: "Can I return early if needed?",
+      answer: "Early returns are possible in case of emergencies.",
     },
     {
       question: "Is the program a paid position?",
-      answer: "Is the program a paid position?",
+      answer:
+        "While not a salaried program, all expenses are covered, and participants gain unparalleled experience and networking opportunities.",
     },
     {
       question: "Where can I go for more information?",
-      answer: "Where can I go for more information?",
+      answer: "Contact us at contact@bitcoinstudentsnetwork.org. ",
+    },
+    {
+      question: "Legal Disclosures",
+      answer:
+        "Please note that legal disclosures, including waivers, will need to be signed prior to participation. These waivers clarify that the Bitcoin Students Network and its affiliates will not be held liable for any personal, financial, or other risks encountered during your involvement in the program. Participants are responsible for their own actions and decisions throughout the experience",
     },
   ];
   return (
