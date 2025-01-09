9b7bc4d53a045821fa38de9844c4cb17fed470e6
Oluwatobi Bamidele <teebams49@gmail.com>
2025-01-09 17:13:43 +0100

    chore: update hero section

--- src/components/layerZero/hero.tsx
+++ src/components/layerZero/hero.tsx
@@ -1,22 +1,27 @@
 import PrimaryButton from "../buttons/primary";
-import H1 from "../h1";
 import localFont from "next/font/local";
 const grandis = localFont({ src: "../../../static-font/Grandis-Bold.woff2" });
+const grandisNormal = localFont({
+  src: "../../../static-font/Grandis-Regular.woff2",
+});
 
 export default function Hero() {
   return (
     <div className="w-full flex items-center justify-center flex-col">
       <div className="lg:w-[50rem] flex items-center justify-center flex-col lg:gap-6 gap-4 lg:py-[8rem] py-[3rem]">
-        <button
-          className={`text-[1.125rem] font-bold text-primary px-6 py-[1.125rem] border-primary border rounded-[2.8125rem] ${grandis.className}`}
-        >
-          LAYER ZERO
-        </button>
-        <h1
-          className={`text-black font-bold text-[1.5rem] lg:text-[4rem] text-center leading-[115%] lg:leading-[4.6rem] w-[19.75rem] lg:w-full ${grandis.className}`}
-        >
-          Building the Foundation for Global Collaboration and Innovation
-        </h1>
+        <div className="flex flex-col items-center justify-center gap-2">
+          <h1
+            className={`text-black font-bold text-[1.5rem] lg:text-[3.5rem] text-center leading-[115%] lg:leading-[4.6rem] w-[19.75rem] lg:w-full ${grandis.className}`}
+          >
+            Layer Zero
+          </h1>
+          <p
+            className={`text-black text-[1.5rem] font-normal text-center leading-[1.8rem] w-[19.75rem] lg:w-[40.75rem] ${grandisNormal.className}`}
+          >
+            Building the Foundation for Global Collaboration and Innovation
+            Through Community Immersion
+          </p>
+        </div>
         <PrimaryButton text="Apply Now" link="#get-involved" />
       </div>
     </div>
